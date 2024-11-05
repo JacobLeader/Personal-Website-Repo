90fe9fef1e4ca2be9c39ea36883a20d91fb37b5f
Jacob <Jacob.leader06@gmail.com>
2024-11-05 14:59:05 -0500

    add underline on hover effect to nav items

--- personal-website/src/app/navbar/navitem.js
+++ personal-website/src/app/navbar/navitem.js
@@ -1,7 +1,7 @@
 export default function Navitem({title, page}) {
     return (
         <a href={`/${page}`}>
-            <div className="font-JetBrains text-darkGray text-2xl px-16">{title}</div>
+            <div className="font-JetBrains text-darkGray text-2xl px-16 swipe-underline">{title}</div>
         </a>
     )
 }
